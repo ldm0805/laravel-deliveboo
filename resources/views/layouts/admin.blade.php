5bb83af290f75de4a7fcbe8a9f5004bcaa62e186
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-03-22 15:24:09 +0100

    lascio icona brutta

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -48,6 +48,9 @@
                 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark navbar-dark sidebar collapse">
                     <div class="position-sticky pt-3">
                         <ul class="nav flex-column">
+                            <div class="bg-danger">
+                                <i class="fa fa-address-book" aria-hidden="true"></i>
+                            </div>
                             {{-- <li class="nav-item">
                                 <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-secondary' : '' }}" href="{{route('admin.dashboard')}}">
                                     <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Dashboard
