935bdea32a062d2b78069a1666ceb8e0cb78e97d
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-03-22 12:37:44 +0100

    Creati model con rcms: -Restaurateur; -Plate; -Order; -Type;

---
+++ app/Http/Controllers/RestaurateurController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Restaurateur;
+use App\Http\Requests\StoreRestaurateurRequest;
+use App\Http\Requests\UpdateRestaurateurRequest;
+
+class RestaurateurController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreRestaurateurRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRestaurateurRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Restaurateur  $restaurateur
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Restaurateur $restaurateur)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Restaurateur  $restaurateur
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Restaurateur $restaurateur)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateRestaurateurRequest  $request
+     * @param  \App\Models\Restaurateur  $restaurateur
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateRestaurateurRequest $request, Restaurateur $restaurateur)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Restaurateur  $restaurateur
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Restaurateur $restaurateur)
+    {
+        //
+    }
+}
