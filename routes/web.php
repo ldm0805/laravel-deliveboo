a181a5a4ec5093300c86b0f212416047967a83ce
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-03-22 16:18:07 +0100

    Aggiunta rotta

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController as DashboardController;
+use App\Http\Controllers\Admin\RestaurateurController as RestaurateurController;
+
 
 
 /*
@@ -25,6 +27,8 @@ Route::get('/', function () {
 // })->middleware(['auth', 'verified'])->name('dashboard');
 Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('restaurateurs', RestaurateurController::class)->parameters(['restaurateurs' => 'restaurateur:slug']);
+
     
 });
 
