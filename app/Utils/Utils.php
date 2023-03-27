c8c053fb81c16a06c149f48fc6da9185e56a4c00
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-03-27 14:45:09 +0200

    aggiunto commento

--- app/Utils/Utils.php
+++ app/Utils/Utils.php
@@ -16,12 +16,12 @@ function changeboolean($boolValue){
 }
 
     // Caambio di immagine
-function displayImage($imagePath, $altText) {
-    $publicPath = public_path('storage/'.$imagePath);
-    if (file_exists($publicPath)) {
-        return '<img class="h-100 w-100" src="'.asset('storage/'.$imagePath).'" alt="'.$altText.'">';
-    } else {
-        return '<img class="h-100 w-100" src="https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg" alt="">';
-    }
-}
+// function displayImage($imagePath, $altText) {
+//     $publicPath = public_path('storage/'.$imagePath);
+//     if (file_exists($publicPath)) {
+//         return '<img class="h-100 w-100" src="'.asset('storage/'.$imagePath).'" alt="'.$altText.'">';
+//     } else {
+//         return '<img class="h-100 w-100" src="https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg" alt="">';
+//     }
+// }
 }
