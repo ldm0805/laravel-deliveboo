ffcdbc3497f6d4e4efcaef28318c0c51d40c2cc9
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-03-29 04:28:06 +0200

    welcome page, header style pudate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,10 +20,8 @@
 </head>
 
 <body>
-    <div class="bg-dark vh-100"id="app">
-
-
-        <nav class="navbar navbar-expand-md navbar-light bg-dark text-white shadow-sm">
+    <div class="my-bg-light my-text-dark vh-100"id="app">
+        <header class="navbar navbar-expand-md navbar-dark my-bg-primary my-text-light shadow-sm my-header">
             <div class="container">
                 <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
                      <div style="transform: scale(1);">
@@ -40,7 +38,7 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="{{url('/') }}">{{ __('Home') }}</a>
+                            <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
                         </li>
                     </ul>
 
@@ -49,16 +47,16 @@
                         <!-- Authentication Links -->
                         @guest
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
                         @if (Route::has('register'))
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                         </li>
                         @endif
                         @else
                         <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link text-white dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
                             </a>
 
@@ -79,9 +77,9 @@
                     </ul>
                 </div>
             </div>
-        </nav>
+        </header>
 
-        <main class="bg-dark">
+        <main class="my-bg-light">
             @yield('content')
         </main>
     </div>
