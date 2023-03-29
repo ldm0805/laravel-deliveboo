65ba07933695d0657141c8a0752701296648620c
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-03-29 18:37:02 +0200

    password update form changed

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -1,51 +1,57 @@
-<section>
-    <header>
-        <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Update Password') }}
-        </h2>
+<section class="update-password">
 
-        <p class="mt-1 text-sm text-gray-600">
-            {{ __('Ensure your account is using a long, random password to stay secure.') }}
-        </p>
-    </header>
+    {{-- Headline --}}
+    <div class="my-profile-headline">
+        <h2>{{ __('Aggiorna Password') }}</h2>
+        <p>{{ __('Assicurati di utilizzare una password sicura.') }} </p>
+    </div>
 
+    {{-- Update Labels --}}
     <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
         @csrf
         @method('put')
 
-        <div class="mb-2">
-            <label for="current_password">{{__('Current Password')}}</label>
+        {{-- Current Password --}}
+        <div class="my-profile-input">
+            <label for="current_password">{{__('Password attuale')}}</label>
             <input class="mt-1 form-control" type="password" name="current_password" id="current_password" autocomplete="current-password">
+
+            {{-- Current Password Error --}}
             @error('current_password')
-            <span class="invalid-feedback mt-2" role="alert">
-                <strong>{{ $errors->updatePassword->get('current_password') }}</strong>
-            </span>
+                <span class="invalid-feedback mt-2" role="alert">
+                    <strong>{{ $errors->updatePassword->get('current_password') }}</strong>
+                </span>
             @enderror
         </div>
 
-        <div class="mb-2">
-            <label for="password">{{__('New Password')}}</label>
-            <input class="mt-1 form-control" type="password" name="password" id="password" autocomplete="new-password">
+        {{-- New Password --}}
+        <div class="my-profile-input">
+            <label for="password">{{__('Nuova Password')}}</label>
+            <input class="form-control" type="password" name="password" id="password" autocomplete="new-password">
+
+            {{-- New Password Error --}}
             @error('password')
-            <span class="invalid-feedback mt-2" role="alert">
-                <strong>{{ $errors->updatePassword->get('password')}}</strong>
-            </span>
+                <span class="invalid-feedback mt-2" role="alert">
+                    <strong>{{ $errors->updatePassword->get('password')}}</strong>
+                </span>
             @enderror
         </div>
 
-        <div class="mb-2">
-
-            <label for="password_confirmation">{{__('Confirm Password')}}</label>
-            <input class="mt-2 form-control" type="password" name="password_confirmation" id="password_confirmation" autocomplete="new-password">
+        {{-- Confirm New Password --}}
+        <div class="my-profile-input">
+            <label for="password_confirmation">{{__('Conferma Nuova Password')}}</label>
+            <input class="form-control" type="password" name="password_confirmation" id="password_confirmation" autocomplete="new-password">
+            {{-- Confirm New Password Error --}}
             @error('password_confirmation')
-            <span class="invalid-feedback mt-2" role="alert">
-                <strong>{{ $errors->updatePassword->get('password_confirmation')}}</strong>
-            </span>
+                <span class="invalid-feedback mt-2" role="alert">
+                    <strong>{{ $errors->updatePassword->get('password_confirmation')}}</strong>
+                </span>
             @enderror
         </div>
 
+        {{-- Save --}}
         <div class="d-flex align-items-center gap-4">
-            <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
+            <button type="submit" class="btn btn-outline-dark">{{ __('Salva') }}</button>
 
             @if (session('status') === 'password-updated')
             <script>
