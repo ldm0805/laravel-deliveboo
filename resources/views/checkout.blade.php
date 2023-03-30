8223c91d0317a6ec6b4e31a1924a5f68ca842fee
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-03-30 11:44:01 +0200

    prova branch

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,4 +1,5 @@
 <form action="/session" method="POST">
     <input type="hidden" name="_token" value="{{csrf_token()}}">
     <button type="submit" id="checkout-live-button">Paga</button>
+    {{-- commento prova --}}
 </form>
