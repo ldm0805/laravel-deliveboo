20935512a16d45f0653334a9ceb5b43c8803de30
Luigi213 <reyesgianluigi88@gmail.com>
2023-04-06 09:42:24 +0200

    fix show ristoranti

--- resources/views/admin/restaurateurs/show.blade.php
+++ resources/views/admin/restaurateurs/show.blade.php
@@ -48,7 +48,31 @@
     </div>
 
     {{-- Plates --}}
-    @include('admin.plates.index')
+    <div class="t-body">
+        @foreach ($plates as $item)
+            @if($restaurateur->id == $item->restaurateur_id)
+                <div class="grid-item t-row grid-container align-items-center py-3 px-3 rounded">
+                    <div class="grid-item">{{$item['name']}}</div>
+                    <div class="grid-item">{{$item['ingredients']}}</div>
+                    <div class="grid-item">{{$item['price']}} &euro;</div>
+                    {!! $utils->changeboolean($item['visible']) !!}
+                    {!! $utils->changeboolean($item['availability']) !!}
+                    <div class="grid-item">{{$item['description']}}</div>
+                    <div class="grid-item d-flex gap-3">
+                        <a href="{{ route('admin.plates.edit', $item) }}" class="text-white"  title="Modifica"><i class="fa-solid fa-pen-to-square"></i></a>
+                        <a href="{{ route('admin.plates.show', $item->slug) }}" class="text-white"  title="Visualizza"><i class="fa-solid fa-eye"></i></a>
+                        <form class="d-inline-block" action="{{route('admin.plates.destroy', $item->slug)}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <a class="text-white p-0 confirm-delete" data-title="{{ $item->name }}" data-title="{{ $item->title }}" data-bs-toggle="modal" data-bs-target="#delete-modal" type="submit" title="Cancella">
+                                <i class="fa-solid fa-dumpster-fire"></i>
+                            </a>
+                        </form> 
+                    </div>
+                </div>
+            @endif
+        @endforeach
+    </div>
 
     @if(session('message'))
         <div class="mt-3">
